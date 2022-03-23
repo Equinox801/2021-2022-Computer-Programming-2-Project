6e663c0ce9350637022bf3abb67f094fbdfaddef
Equinox 801 <90404517+Equinox801@users.noreply.github.com>
2022-03-22 20:28:31 -0600

    Add files via upload

---
+++ src/Main (8).java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+class Main {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame();
+    frame.setSize(300, 466);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+    frame.getContentPane().setBackground(Color.BLACK);
+
+    VectoralEntity test = new VectoralEntity();
+    
+  }
+  
+  //Distance between VectoralEntities Player and Projectile
+  public static double getDistance(){
+    
+  }
+}
